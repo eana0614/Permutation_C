8f8d905fbed13018c8053e6916ec3a580d8c00df
eana0614 <eana0614@DESKTOP-C7AKB6O>
2017-04-25 00:27:53 +0900

    Modify permutation.c

--- Permutation/Permutation/permutation.c
+++ Permutation/Permutation/permutation.c
@@ -2,28 +2,89 @@
 
 #include<stdio.h>
 #include<memory.h>
+#include<string.h>
 
 int checkingInputString(char* input, int length);
+int factorial(int i);
+void swap(char* input, int i, int j);
+void permutation(char* input, int length, int n);
+void printPermutationElement(int length);
+
+char** perElement;
+int count = 0;
 
 int main(void) {
 
-	char* input;
+	char input[1000];
 	int length = 0;
+	int permutationNumber = 0;
 
-	printf("!] INPUT DIFFERENT N NUMBERS OR CHARACTERS.\n");
+	printf("!] INPUT DIFFERENT N NUMBERS OR CHARACTERS. (Max length 99)\n");
 	printf("   ex) 1234 or abcse \n");
 
 	scanf("%s", &input);
-	length = sizeof(input) / sizeof(char);
+	length = strlen(input);
 
 	if (checkingInputString(input, length) > 0) {
 
+		permutationNumber = factorial(length);
+		perElement = (char**)malloc(sizeof(char*)*permutationNumber);
+
+		for (int i = 0; i < permutationNumber; i++) {
+			perElement[i] = (char*)malloc(length+1);
+		}
+
+		permutation(input, length, length);
+		printPermutationElement(permutationNumber);
+
+		printf("!] Done. \n");
 	}
 	else {
 		printf("!] Error. Incorrect input.");
 		exit(1);
 	}
 	
+}
+
+void printPermutationElement(int length) {
+
+	printf("========== Result ==========\n");
+
+	for (int i = 0; i < length; i++) {
+		printf("%dth permutation element = %s \n", (i+1), perElement[i]);
+	}
+
+	printf("\n");
+}
+
+void permutation(char* input, int length, int n) {
+
+	if (n == 1) {
+		strcpy(perElement[count], input);
+		count++;
+	}
+
+	for (int i = 0; i < n; i++) {
+		swap(input, i, n - 1);
+		permutation(input, length, n - 1);
+		swap(input, i, n-1);
+	}
+}
+
+void swap(char* input, int i, int j) {
+	if (i != j) {
+		char temp = input[i];
+		input[i] = input[j];
+		input[j] = temp;
+	}
+}
+
+int factorial(int i) {
+	if (i <= 1) {
+		return 1;
+	}
+
+	return i * factorial(i-1);
 }
 
 int checkingInputString(char* input, int length) {
